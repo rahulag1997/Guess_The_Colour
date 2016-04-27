706601a7b339cc975420e581ef5b08b51af14715
rahulag1997 <rahulag1997@gmail.com>
2016-04-27 11:54:37 +0530

    ads added

--- app/src/main/java/com/example/rahul/guessthecolour/Game.java
+++ app/src/main/java/com/example/rahul/guessthecolour/Game.java
@@ -13,7 +13,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import static java.lang.Math.random;
 import static java.lang.Math.round;
